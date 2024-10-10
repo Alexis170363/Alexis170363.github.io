39b2c818593f6bd8f9252f70d842f7f5e8fef75f
Alexis170363 <moisesalexis1703@gmail.com>
2024-10-09 22:54:57 -0500

    Add files via upload

---
+++ Proyecto/Javas/Scrip.js
@@ -0,0 +1,8 @@
+function abrirWhatsApp() {
+    var numeroTelefono = "+573235959901"; // reemplaza con el número de teléfono destino
+    var url = "(link unavailable)" + numeroTelefono;
+    window.open(url, "_blank");
+  }
+  
+  <button onclick="abrirWhatsApp()">Enviar mensaje a WhatsApp</button>
+  
